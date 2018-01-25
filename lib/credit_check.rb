f6a788e0654cbb08d3a7853d07841c9d4211666a
jrambold <jrambold@gmail.com>
2018-01-25 11:22:35 -0700

    separated program into class

--- lib/credit_check.rb
+++ lib/credit_check.rb
@@ -1,23 +1,32 @@
-cardnum = ARGV[0]
-cardnum = cardnum.split("").map { |cardnum| cardnum.to_i }
+class Credit_Check
 
-sum = 0
-digits = cardnum.length
-odd_check = digits % 2
+  def initialize(cardnum)
+    @cardnum = cardnum.split("").map { |cardnum| cardnum.to_i }
+  end
 
-for i in 0..(digits - 2) do
-    if i%2 == odd_check
-      cardnum[i] = cardnum[i] * 2
+  def is_valid?
+    sum = 0
+    digits = @cardnum.length
+    odd_check = digits % 2
+
+    for i in 0..(digits - 2) do
+        if i%2 == odd_check
+          @cardnum[i] = @cardnum[i] * 2
+        end
+        if @cardnum[i] > 9
+          @cardnum[i] = @cardnum[i] - 9
+        end
+        sum = sum + @cardnum[i]
     end
-    if cardnum[i] > 9
-      cardnum[i] = cardnum[i] - 9
-    end
-    sum = sum + cardnum[i]
+
+    sum *= 9
+    return (sum%10 == @cardnum.last)
+  end
 end
-sum *= 9
-valid = (sum%10 == cardnum.last)
 
-if valid
+card = Credit_Check.new(ARGV[0])
+
+if card.is_valid?
   puts "The number is valid!"
 else
   puts "The number is invalid!"
